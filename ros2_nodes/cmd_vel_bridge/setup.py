df4fdf80e3af819adb3ac5b2ec68f5453b9db15d
zdz <zdz@macm4.3ds.com>
2025-12-28 08:03:11 -0800

    feat: add cmd_vel_bridge ROS2 package structure

---
+++ ros2_nodes/cmd_vel_bridge/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'cmd_vel_bridge'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='LanderPi',
+    maintainer_email='user@example.com',
+    description='cmd_vel to motor bridge for LanderPi mecanum robot',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'cmd_vel_bridge = cmd_vel_bridge.cmd_vel_bridge_node:main',
+        ],
+    },
+)
